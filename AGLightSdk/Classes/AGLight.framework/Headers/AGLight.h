a8da63bc1909483ac2f0d24587807b68bda67084
AliasCocoa <cocoaalias@gmail.com>
2017-12-13 11:36:15 +0100

    add frameWork

---
+++ AGLightSdk/Classes/AGLight.framework/Headers/AGLight.h
@@ -0,0 +1,21 @@
+//
+//  AGLight.h
+//  AGLight
+//
+//  Created by Adel on 13/12/2017.
+//  Copyright © 2017 Alias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AGLight.
+FOUNDATION_EXPORT double AGLightVersionNumber;
+
+//! Project version string for AGLight.
+FOUNDATION_EXPORT const unsigned char AGLightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGLight/PublicHeader.h>
+
+#import <AGLight/AGService.h>
+
+
---
+++ AGLightSdk/Classes/AGLight.framework/Headers/AGService.h
@@ -0,0 +1,17 @@
+//
+//  AGService.h
+//  AGLight
+//
+//  Created by Adel on 13/12/2017.
+//  Copyright © 2017 Alias. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGService : NSObject
+    
+    
+    
+- (void)getConfig;
+
+@end
